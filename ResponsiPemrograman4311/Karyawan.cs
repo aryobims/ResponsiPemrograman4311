742335db9779d1b832dca4cbbc65a3d64c9f0e9b
aryobims <aryobimo@students.amikom.ac.id>
2022-04-19 16:47:38 +0700

    responsi

---
+++ ResponsiPemrograman4311/Karyawan.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4311
+{
+    public class Karyawan
+    {
+        private string nik;
+        private string nama;
+        private int gajibulanan;
+
+        public string Nik
+        {
+            get { return nik; }
+            set { nik = value; }
+        }
+        public string Nama
+        {
+            get { return nama; }
+            set { nama = value; }
+        }
+        public int GajiBulanan
+        {
+            get { return gajibulanan; }
+            set { gajibulanan = value; }
+        }
+
+        public Karyawan(string nik, string nama, int gajiBulanan)
+        {
+            this.nik = nik;
+            this.nama = nama;
+            this.gajibulanan = gajiBulanan < 0 ? 0 : gajiBulanan;
+        }
+
+        public void tampilkanData()
+        {
+            Console.WriteLine("{0} {1}      {2}", nik, nama, gajibulanan);
+        }
+
+        public void kenaikan()
+        {
+            int kenaikan;
+
+            kenaikan = gajibulanan * 110;
+            gajibulanan = kenaikan / 100;
+
+            Console.WriteLine("woooow naik gaji 10%");
+        }
+    }
+}
---
+++ ResponsiPemrograman4311/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4311
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek 
+            Karyawan Karyawan1 = new Karyawan ("3404102002900007", "Paiman", 5000000);
+            Karyawan Karyawan2 = new Karyawan("34041030040010007", "Judhit", 4000000);
+
+            //menampilkan data
+            Console.WriteLine("Aryo Bimo/21.11.4311/21-IF-07");
+            Console.WriteLine("Nik/Nama      Gaji Bulanan");
+            Console.WriteLine("-----------------------------");
+            Console.Write("1 "); Karyawan1.tampilkanData();
+            Console.Write("2 "); Karyawan2.tampilkanData();
+            Console.WriteLine();
+
+            //pemanggilan method
+            Karyawan1.kenaikan();
+            Karyawan2.kenaikan();
+
+            Console.WriteLine();
+            Console.WriteLine("Nik/Nama      Gaji Bulanan");
+            Console.WriteLine("-----------------------------");
+            Console.Write("1 "); Karyawan1.tampilkanData();
+            Console.Write("2 "); Karyawan2.tampilkanData();
+
+            Console.ReadKey();
+        }
+    }
+}
